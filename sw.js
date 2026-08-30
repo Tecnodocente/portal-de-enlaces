c0a2b8840f81acc3b17ef5706857f2294e02617b
Francisco Javier Virues Vallejo <fvirval243@g.educaand.es>
2026-08-30 02:39:30 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 const CACHE_PREFIX = 'portal-de-enlaces-';
-const SERVICE_WORKER_VERSION = '2.4.3';
+const SERVICE_WORKER_VERSION = '2.4.4';
 const CACHE_NAME = CACHE_PREFIX + SERVICE_WORKER_VERSION;
 const SHELL_URLS = [
   './',
